8703c8f884e65f9037bfc32afcd54a7710a9387a
Kelly <jenkins0703@hotmail.com>
2019-12-03 18:44:39 -0700

    after tutor

--- PWgeneraroe.js
+++ PWgeneraroe.js
@@ -3,9 +3,9 @@ let lowL = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 let number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 let special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 
-function getpasswordoptions() {
+function getPasswordOptions() {
     let length = parseInt(prompt(
-        "How many characters would you like yourpassword to contain? (between 8-128)"
+        "How many characters would you like your password to contain? (between 8-128)"
     ))
     if (isNaN(length) === true) {
         alert("Your paswword length must be a number")
@@ -17,22 +17,119 @@ function getpasswordoptions() {
         return;
     }
 
-    if (length > 128) {
+    if (length > 128){
         alert("This is too many characters.  Less than 129, please")
-        return;
+        return; 
     }
 
     let hasSpecialCharacters = confirm ("Click ok to confirm special charaters");
     let hasNumericCharacters = confirm ("Click ok to confirm numeric charaters");
     let hasUpperCharacters = confirm ("Click ok to confirm upper cased charaters");
     let hasLowerCharacters = confirm ("Click ok to confirm lower cased charaters");
+
+    if ( hasLowerCharacters === false && hasNumericCharacters === false && hasSpecialCharacters ===false && hasUpperCharacters ===false) {
+        alert("Please select at least one type")
+        return; 
+    }
+    let passwordOptions = {
+        length: length,
+        hasLowerCharacters: hasLowerCharacters,
+        hasNumericCharacters: hasNumericCharacters,
+        hasSpecialCharacters: hasSpecialCharacters,
+        hasUpperCharacters: hasUpperCharacters
+    }
+    return passwordOptions
+}
+function getRandom(arr) {
+    let randomIndex = Math.floor (Math.random () * arr.length);
+    let randomElement = arr [randomIndex];
+    return randomElement
 }
 
-getpasswordoptions();
+function generatePassword () {
+    let options = getPasswordOptions ();
+    let result = [];
+    let possibleCharacters = [];
+    let gauranteedCharacters = [];
+
+    if (options.hasSpecialCharacters) {
+        possibleCharacters = possibleCharacters.concat(special)
+        gauranteedCharacters.push (getRandom(special));
+    }
+    if (options.hasNumericCharacters) {
+        possibleCharacters = possibleCharacters.concat(number)
+        gauranteedCharacters.push (getRandom(number));
+    }
+    if (options.hasUpperCharacters) {
+        possibleCharacters = possibleCharacters.concat(capL)
+        gauranteedCharacters.push (getRandom(capL));
+    }
+    if (options.hasLowerCharacters) {
+        possibleCharacters = possibleCharacters.concat(lowL)
+        gauranteedCharacters.push (getRandom(lowL));
+    }
+
+    for (let i = 0; i < options.length; i++) {
+        let possibleCharacter = getRandom (possibleCharacters)
+        result.push  (possibleCharacter)
+    }
+    for (let i = 0; i < gauranteedCharacters.length; i++) {
+        result [i] = gauranteedCharacters [i]
+        
+    }
+    return result.join ("")
+    
+}
+let copyButton = document.querySelector ("#copy");
+let generateButton = document.querySelector ("#generate");
+
+
+function writePassword ()  {
+    let password = generatePassword ();
+    let passwordText = document.querySelector ("#password");
+    passwordText.value = password;
+    copyButton.removeAttribute ("disabled");
+    copyButton.focus ();
+
+function copyToClipboard() {
+    var passwordText = document.querySelector("#password");
+        passwordText.select();
+        document.execCommand("copy");
+        alert(
+          "Your password " + passwordText.value + " was copied to your clipboard."
+        );
+      }
+// Add event listener to generate button
+      generateBtn.addEventListener("click", writePassword);
+      // Add event listener to copy button
+      copyBtn.addEventListener("click", copyToClipboard);
+
+}
+
+
+
+
+// arrays for options
+
+//Create pwOption[]                                  
+
+//if (hasSpecialCharacters === true) {
+   //// pwoption = pwoption.concat(capL)
+//}
+//if (hasSpecialCharacters === true) {
+//    pwoption = pwoption.concat(number)
+//}
+//if (hasSpecialCharacters === true) {
+ //   pwoption = pwoption.concat(lowL)
+//}
+//if (hasSpecialCharacters === true) {
+ //   pwoption = pwoption.concat(special)
+//}
+
 
 // I think I need a for loop to for the number of characters chosen.  
 
-    //for (let length > 0; index < array.length; index++) {
+    //for (let length > 0; length < array.length; index++) {
         //const element = array[index];
         
     //}
